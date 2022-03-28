a08e6074d17fddab5a7ba73431af387b3792f199
jcarty <jcarty@DESKTOP-7CK77VG>
2022-03-28 11:15:59 +0200

    progress so far

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,15 +10,14 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-impl AppendBar for Vector {
-    fn append_bar(self) -> Vector {
-
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Vec<String> {
+        self.push(String::from("Bar"));
+        self
     }
 
 }
